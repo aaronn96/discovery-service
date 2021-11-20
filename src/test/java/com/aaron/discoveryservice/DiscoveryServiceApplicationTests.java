4fe4f7417418d2c0a2add75f97bf378a9a679ead
aaronn96 <aprades96@gmail.com>
2021-11-20 17:59:28 +0100

    Initial commit

---
+++ src/test/java/com/aaron/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaron.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
